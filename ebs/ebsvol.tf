fa541d9b0579988b282c3b48a9c6b99392e4c669
Aditya Pal <123142654+pal-aditya@users.noreply.github.com>
2025-11-27 00:02:09 +0530

    Updated with instance + security group

--- ebs/ebsvol.tf
+++ ebs/ebsvol.tf
@@ -9,9 +9,57 @@ resource "aws_ebs_volume" "restart_ebs" {
   type              = "gp2"
 }
 
+data "aws_ami" "ubuntu"{
+  most_recent = true
+  owners = ["099720109477"]
+
+  filter {
+    name = "name"
+    value = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    value = ["hvm"]
+  }
+
+  filter {
+    name = "root-device-type"
+    value = ["ebs"]
+  }
+}
+
+locals{
+  inbound = [22,80]
+  cidr_block = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group" "restart_sg"{
+  name = "dynamic-sg"
+  vpc_id = var.vpc_id
+  dynamic "ingress" {
+    for_each = local.inbound
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "tcp"
+      cidr_blocks = local.cidr_block
+    }
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = local.cidr_block
+  }
+
+}
+
 resource "aws_instance" "restart_instance" {
-  ami           = "ami-02b8269d5e85954ef"
+  ami_filter 
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.restart_sg.id]
 }
 
 resource "aws_volume_attachment" "restart"{
