df7928295779425f98890def638c1c3c58c0deba
Aditya Pal <123142654+pal-aditya@users.noreply.github.com>
2025-12-02 12:15:40 +0530

    example student table

---
+++ dynamodb/dynamoexample.tf
@@ -0,0 +1,45 @@
+resource "aws_dynamodb_table" "dec_restart"{
+    name = "student"
+    billing_mode = "pay_per_request"
+    hash_key = "student_id"
+
+#Attributes are like coloumns in tables
+
+    attribute {
+        name = "student_id"
+        type = "N"
+    }
+
+    attribute {
+        name = "student_name"
+        type = "S"
+    }
+
+    attribute {
+        name = "Class"
+        type = "S"
+    }
+
+#ttl (attribute_name - (Optional) Name of the table attribute to store the TTL timestamp in. Required if enabled is true, must not be set otherwise.)
+
+    ttl {
+        attribute_name = "ttl"
+        enabled = true
+    }   
+    point_in_time_recovery{
+        enabled = true
+    }
+
+    server_side_encryption {
+        enabled = true
+        kms_key_arn = "arn:aws:kms:ap-south-1:676290432212:key/e6cc1d38-2c6a-40b7-922b-cfaeab982bbc"
+    }
+
+# Two Options for table class:- Standard(Default),DynamoDB STandard Infrequent Access [Where the storage cost is dominant and we don't required data frequently]
+
+    table_class = "STANDARD"
+
+    tags = {
+        name = "DynamoDBexample"
+    }
+}
